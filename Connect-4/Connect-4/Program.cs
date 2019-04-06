c2b967ad7ea99de61aae062b3405e17586fed11e
NemethKrisztian96 <n.krisztian96@gmail.com>
2019-04-06 22:29:04 +0300

    Added State class

--- Connect-4/Connect-4/Program.cs
+++ Connect-4/Connect-4/Program.cs
@@ -10,6 +10,29 @@ namespace Connect_4
     {
         static void Main(string[] args)
         {
+            BoardTest();
+        }
+
+        private static void BoardTest()
+        {
+            State state = new State();
+
+            for (int i = 0; i < 20; i++)
+            {
+                while (!state.CanAddToBoard(i % 7))
+                {
+                    i++;
+                }
+                state.AddToBoard(i % 7, State.FIELD.MAX);
+                ++i;
+                while (!state.CanAddToBoard(i % 7))
+                {
+                    i++;
+                }
+                state.AddToBoard(i % 7, State.FIELD.MIN);
+            }
+
+            state.PrintBoard();
         }
     }
 }
